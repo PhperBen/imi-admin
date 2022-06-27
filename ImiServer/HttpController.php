d89ccbb7ca6f4e839bb7a0f2308a488cb7351053
Saopig <87409645@qq.com>
2022-06-27 06:25:44 +0000

    跟随imi最新修复

--- ImiServer/HttpController.php
+++ ImiServer/HttpController.php
@@ -4,28 +4,20 @@ declare(strict_types=1);
 
 namespace ImiApp\ImiServer;
 
-use Imi\Server\Contract\IServer;
 use Imi\Aop\Annotation\Inject;
 
-class HttpController
+class HttpController extends \Imi\Server\Http\Controller\HttpCOntroller
 {
     /**
      * 响应
      * @Inject(Response::class)
      */
-    public $response;
+    public \Imi\Server\Http\Message\Contract\IHttpResponse $response;
 
     /**
      * 请求
      * @Inject(Request::class)
      */
-    public $request;
+    public \Imi\Server\Http\Message\Contract\IHttpRequest $request;
 
-    public function __construct(IServer $server)
-    {
-        $this->server = $server;
-        if (method_exists($this, '__init')) {
-            $this->__init();
-        }
-    }
 }
