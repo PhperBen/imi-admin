bed0fce81e1192cc0b006bf52411ce56e3800999
Saopig <87409645@qq.com>
2022-07-24 10:57:16 +0000

    修复文件上传js

--- imi-ui/src/config/upload.js
+++ imi-ui/src/config/upload.js
@@ -10,8 +10,8 @@ export default {
 	parseData: function (res) {
 		return {
 			code: res.code,				//分析状态字段结构
-			fileName: res.data.filename,//分析文件名称
-			src: res.data.url,			//分析图片远程地址结构
+			fileName: res.data ? res.data.filename : null,//分析文件名称
+			src: res.data ? res.data.url : null,			//分析图片远程地址结构
 			msg: res.message			//分析描述字段结构
 		}
 	},
